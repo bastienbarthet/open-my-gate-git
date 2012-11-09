7bccfbb471324be6536efb494287d1507768b5a5
bastien.barthet@gmail.com <bastien.barthet@gmail.com@2db9f4b8-6408-874f-3ccd-cd3bf4ed9050>
2012-11-09 21:48:32 +0000

    the app is now a servcie
    git-svn-id: https://open-my-gate.googlecode.com/svn/trunk@12 2db9f4b8-6408-874f-3ccd-cd3bf4ed9050

--- src/com/baba/openmygate/GateLocationListener.java
+++ src/com/baba/openmygate/GateLocationListener.java
@@ -1,38 +1,63 @@
 package com.baba.openmygate;
 
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.Toast;
 
-public class GateLocationListener implements LocationListener {
+public class GateLocationListener extends Activity implements LocationListener {
 
-
-	private TextView latitudeField;
-	private TextView longitudeField;
+	private final double gateLatitude = 43.61206;
+	private final double gateLongitude = 1.39245;
+	private final double radius = 50; //radius detection in meters
+	private final String numTel = "+33689838373";
+	private double distance;
+	private boolean gateOpened;
+	private double latitude;
+	private double longitude;
+	private boolean gpsStatus;
 	
-	public GateLocationListener(TextView latitudeField, TextView longitudeField) {
-		this.latitudeField = latitudeField;
-		this.longitudeField = longitudeField;
+	public GateLocationListener() {
+		super();
+		this.gateOpened = false;
 	}
 	
 	@Override
 	public void onLocationChanged(Location location) {
-		this.latitudeField.setText(String.valueOf(location.getLatitude()));
-		this.longitudeField.setText(String.valueOf(location.getLongitude()));
-
+		 
+		this.latitude = location.getLatitude();
+		this.longitude = location.getLongitude();
+		
+		if (isNearToGate(longitude, latitude)) {
+			if (!this.gateOpened) {
+				this.gateOpened = true;;
+				openGate();
+			}
+		} else {
+			this.gateOpened = false;
+		}
+		
+		
+	 	Toast.makeText(getBaseContext(),
+	 			"Position : " + this.latitude + " " + this.longitude + "\n"
+	 			+ "Distance : " + this.distance + "\n"
+	 			+ "Gate Opened ? : " + this.gateOpened + "\n"
+	 			+ "GPS Status : " + this.gpsStatus, 
+	 			Toast.LENGTH_LONG).show();
 	}
 
 	@Override
 	public void onProviderDisabled(String provider) {
-		// TODO Auto-generated method stub
-
+		this.gpsStatus = false;
 	}
 
 	@Override
 	public void onProviderEnabled(String provider) {
-		// TODO Auto-generated method stub
-
+		this.gpsStatus = true;
 	}
 
 	@Override
@@ -40,5 +65,28 @@ public class GateLocationListener implements LocationListener {
 		// TODO Auto-generated method stub
 
 	}
+	
+    private void openGate() {
+    	try {
+            Intent callIntent = new Intent(Intent.ACTION_CALL);
+            callIntent.setData(Uri.parse("tel:"+this.numTel));
+            startActivity(callIntent);
+        } catch (ActivityNotFoundException e) {
+            //Log.e("dialing", "Call failed", e);
+        }
+    }
+    
+    private boolean isNearToGate(double longitude, double latitude) {
+    	final double deltaLatitude = latitude-this.gateLatitude;
+    	final double deltaLongitude = longitude-this.gateLongitude;
+    	
+    	this.distance = Math.sqrt(deltaLatitude*deltaLatitude + deltaLongitude*deltaLongitude)*111.11*1000;
+    	
+    	boolean result = false;
+    	if (this.distance < this.radius) {
+    		result = true;
+    	}
+    	return result;
+    }
 
 }
--- src/com/baba/openmygate/OpenMyGate.java
+++ src/com/baba/openmygate/OpenMyGate.java
@@ -1,14 +1,10 @@
 package com.baba.openmygate;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
+import android.app.ActivityManager;
+import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Context;
 import android.content.Intent;
-import android.location.Criteria;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -16,148 +12,73 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class OpenMyGate extends Activity implements LocationListener{
+public class OpenMyGate extends Activity {
 
-	private TextView latitudeField;
-	private TextView longitudeField;
-	private TextView openGateField;
-	private TextView distanceField;
-	private TextView gpsStatus;
 	private Button launchButton;
-	private LocationManager locationManager;
-	private String provider;
-	private final double gateLatitude = 43.61206;
-	private final double gateLongitude = 1.39245;
-	private final double radius = 50; //radius detection in meters
-	private final String numTel = "+33689838373";
-	  
+	private Button stopButton;
+	private TextView serviceStatus;
 	 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        setContentView(R.layout.activity_main);
         
-        latitudeField = (TextView) findViewById(R.id.textLatitude);
-        longitudeField = (TextView) findViewById(R.id.textLongitude);
-        openGateField = (TextView) findViewById(R.id.textOpenGate);
-        distanceField = (TextView) findViewById(R.id.textDistance);
-        gpsStatus = (TextView) findViewById(R.id.textGpsStatus);
+        setContentView(R.layout.activity_main);
+  
         launchButton = (Button) findViewById(R.id.button1);
+        stopButton = (Button) findViewById(R.id.button2);
+        serviceStatus = (TextView) findViewById(R.id.textServiceStatus);
         
         launchButton.setOnClickListener( 
         		
         	new OnClickListener(){
-
 	        	 @Override
 	        	 public void onClick(View actuelView) {
 	        		 startService(new Intent(OpenMyGate.this, OpenMyGateService.class));
+	        		 isMyServiceRunning();
 	        	 }
-	        	 
-        	}
-        );
+        	});
         
-        // Get the location manager
-        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        // Define the criteria how to select the locatioin provider -> use
-        // default
-        Criteria criteria = new Criteria();
-        provider = locationManager.getBestProvider(criteria, false);
-        Location location = locationManager.getLastKnownLocation(provider);
-
-        // Initialize the location fields
-        if (location != null) {
-          System.out.println("Provider " + provider + " has been selected.");
-          onLocationChanged(location);
-          gpsStatus.setText("ON");
-        } else {
-          latitudeField.setText("Location not available");
-          longitudeField.setText("Location not available");
-          gpsStatus.setText("OFF");
+        stopButton.setOnClickListener( 
+        	new OnClickListener(){
+	        	 @Override
+	        	 public void onClick(View actuelView) {
+	        		 stopService(new Intent(OpenMyGate.this, OpenMyGateService.class));
+	        		 isMyServiceRunning();
+	        	 }
+        	});
+        
+        this.isMyServiceRunning();
+        
+    }
+    
+    @Override
+    protected void onResume() {
+    	super.onResume();
+    	this.isMyServiceRunning();
+    	
+    };
+    
+    private boolean isMyServiceRunning() {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (OpenMyGateService.class.getName().equals(service.service.getClassName())) {
+            	this.launchButton.setEnabled(false);
+            	this.stopButton.setEnabled(true);
+            	this.serviceStatus.setText("ON");
+                return true;
+            }
         }
-        
+        this.launchButton.setEnabled(true);
+    	this.stopButton.setEnabled(false);
+    	this.serviceStatus.setText("OFF");
+        return false;
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-//        getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
     
-    private void openGate() {
-    	try {
-            Intent callIntent = new Intent(Intent.ACTION_CALL);
-            callIntent.setData(Uri.parse("tel:"+this.numTel));
-            startActivity(callIntent);
-        } catch (ActivityNotFoundException e) {
-            //Log.e("helloandroid dialing example", "Call failed", e);
-        }
-    }
-    
-    private boolean isNearToGate(double longitude, double latitude) {
-    	final double deltaLatitude = latitude-this.gateLatitude;
-    	final double deltaLongitude = longitude-this.gateLongitude;
-    	
-    	final double distance = Math.sqrt(deltaLatitude*deltaLatitude + deltaLongitude*deltaLongitude)*111.11;
-    	this.distanceField.setText(String.valueOf(distance*1000));
-    	
-    	boolean result = false;
-    	if (distance*1000 < radius) {
-    		result = true;
-    	}
-    	return result;
-    }
 
-    /* Request updates at startup */
-    @Override
-    protected void onResume() {
-      super.onResume();
-      locationManager.requestLocationUpdates(provider, 1*1000, 1, this);
-    }
-
-    /* Remove the locationlistener updates when Activity is paused */
-    @Override
-    protected void onPause() {
-      super.onPause();
-      //locationManager.removeUpdates(this);
-    }
-
-    
-	@Override
-	public void onLocationChanged(Location location) {
-		
-		this.onResume();
-		
-		final double latitude = location.getLatitude();
-		final double longitude = location.getLongitude();
-		
-		if (isNearToGate(longitude, latitude)) {
-			if (!this.openGateField.getText().equals("YES")) {
-				this.openGateField.setText("YES");
-				openGate();
-			}
-		} else {
-			this.openGateField.setText("NO");
-		}
-		
-		this.latitudeField.setText(String.valueOf(latitude));
-		this.longitudeField.setText(String.valueOf(longitude));
-		
-	}
-
-	@Override
-	public void onProviderDisabled(String provider) {
-		this.gpsStatus.setText("OFF");
-	}
-
-	@Override
-	public void onProviderEnabled(String provider) {
-		this.gpsStatus.setText("ON");
-	}
-
-	@Override
-	public void onStatusChanged(String provider, int status, Bundle extras) {
-		// TODO Auto-generated method stub
-		
-	}
 }
